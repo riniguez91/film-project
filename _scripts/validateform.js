8ec979fc85c28f5d9bdd5cf4c1456b023e5162b6
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2020-12-14 11:45:27 +0100

    Corregido errores script validar registro usuario

--- _scripts/validateform.js
+++ _scripts/validateform.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-    $("#basic-form").validate({
-      rules: {
-        firstname : {
-          required: true,
-          minlength: 2
-        },
-        lastname: {
-          required: true,
-          minlength: 2
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        },
-      messages : {
-        name: {
-          minlength: "Enter your name..."
-        },
-        age: {
-          minlength: "Enter yout surname..."
-        },
-        email: {
-          email: "Format must be: abc@domain.tld"
-        }
+  $("#basic-form").validate({
+    rules: {
+      fname : {
+        required: true,
+        minlength: 2
+      },
+      lname: {
+        required: true,
+        minlength: 2
+      },
+      email: {
+        required: true,
+        email: true
+      },
+    messages : {
+      fname: {
+        minlength: "Enter your name..."
+      },
+      age: {
+        minlength: "Enter yout surname..."
+      },
+      email: {
+        email: "Format must be: abc@domain.tld"
       }
-    });
+    }
+    }
   });
+})
