97b07e7011e42035af263ddbd5027b1b57c00f29
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2021-01-09 18:54:14 +0100

    Fixed home carousel bugs and implemented "full" page films on billboard

--- _scripts/card-carousel.js
+++ _scripts/card-carousel.js
@@ -1,3 +1,4 @@
+/* Allows for multi-carrousels to work without disturbing each other  */
 $(document).ready(function() {
     $("#actorCarousel, #directorCarousel").on("slide.bs.carousel", function(e) {
       var $e = $(e.relatedTarget);
@@ -9,44 +10,17 @@ $(document).ready(function() {
       if (idx >= totalItems - (itemsPerSlide - 1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i = 0; i < it; i++) {
-          // append slides to end
+          // Append slides to end
           if (e.direction == "left") {
             $("#" + curr_id + " .carousel-item")
               .eq(i)
-              .appendTo(".carousel-inner");
+              .appendTo("#" + curr_id + " .carousel-inner");
           } else {
             $("#" + curr_id + " .carousel-item")
               .eq(0)
-              .appendTo(".carousel-inner");
+              .appendTo("#" + curr_id + " .carousel-inner");
           }
         }
       }
     });
   });
-
-  /* 
-  $(document).ready(function() {
-    $("#actorCarousel").on("slide.bs.carousel", function(e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 4;
-      var totalItems = $("#actorCarousel.carousel-item").length;
-  
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-          // append slides to end
-          if (e.direction == "left") {
-            $("#actorCarousel.carousel-item")
-              .eq(i)
-              .appendTo("#actorCarousel.carousel-inner");
-          } else {
-            $("#actorCarousel.carousel-item")
-              .eq(0)
-              .appendTo($(this).find("#actorCarousel.carousel-inner"));
-          }
-        }
-      }
-    });
-  });
-  */
