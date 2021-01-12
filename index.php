a0c85a12c633583c2729c2ae2da651c009b1a24f
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2021-01-12 16:25:05 +0100

    Implemented login and navbar backend
    Added database folder

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -15,44 +14,13 @@
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css'>
     </head>
 
-    <body> 
-      <!-- NAVBAR -->
-      <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-        <a class="navbar-brand" href="#">
-          <img src="_images/navbaricon_fp.jpg" width="45" height="40" style="border-radius: 5px">
-        </a>
-        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-          <!--Home-->
-          <div class="navbar-nav">
-            <a class="nav-item nav-link px-3 active" href="index.html">
-              <img src="https://www.flaticon.es/svg/static/icons/svg/1692/1692828.svg" height="18"> Home
-            </a>
-            <!--Billboard-->
-            <li class="dropdown">
-              <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                <img src="https://www.flaticon.com/svg/static/icons/svg/2433/2433310.svg" height="23"> Billboard <!-- https://www.flaticon.com/svg/static/icons/svg/3989/3989664.svg -->
-             </a>
-              <ul class="dropdown-menu">
-                  <li class="dropdown"><a href="_html/_billboard/action.html" class="drop">Action </a></li>
-                  <li class="dropdown"><a href="_html/_billboard/adventure.html" class="drop">Adventure </a></li>
-                  <li class="dropdown"><a href="_html/_billboard/comedy.html" class="drop">Comedy </a></li>
-                  <li class="dropdown"><a href="_html/_billboard/scifi.html" class="drop">Science Fiction </a></li>
-              </ul>
-            </li>
-            <!--About Us-->
-            <a class="nav-item nav-link px-3 active" href="_html/about_us.html">
-              <img src="https://www.flaticon.com/svg/static/icons/svg/788/788927.svg" height="20"> About us
-            </a>
-            <!--Login-->
-            <a class="nav-item nav-link px-3 active" href="_html/subscribe.html">
-              <img src="https://www.flaticon.com/svg/static/icons/svg/1092/1092848.svg" height="19"> Login
-            </a>
-          </div>
-        </div>
-      </nav>  
+    <body>  
+      <?php
+      if (session_status() == 0) {
+        session_start();
+      }
+      include("_php/navbar.php") 
+      ?> 
         
         <div class="bd-example">
           <div class="embed-responsive embed-responsive-16by9">
